a634264858b0d3471774f1b9dca5f0458bd18b20
bartelka <tanicka879gmail.com>
2022-12-06 16:23:11 +0100

    koncne som dorobila 2ku

--- hada/hada.py
+++ hada/hada.py
@@ -3,13 +3,29 @@ max = 0
 pocet_hier = 0
 kopia_fr = open("hada_kopia.txt", "w", encoding="utf-8")
 for riadok in fr:
-    dlzka = len(riadok)
+    dlzka = len(riadok)-1
     pocet_hier += 1
-    pocet = 0
-    zoz = []
-    for i in riadok:
-       pocet += 1
-       if pocet > max:
-            max = pocet
+    if dlzka > max:
+        max = dlzka
+fr = open("hada.txt", "r", encoding="utf-8")
+for riadok in fr:
+    poc = 0
+    zoz = list(riadok)
+    pismenko = zoz[0]
+    hotovecka = ""
+    taky_zoz = []
+    for i in range(len(riadok)):
+        poc += 1
+        if pismenko != zoz[0]:
+            if poc > len(taky_zoz):
+                poc -= 1
+            hotovecka = hotovecka + pismenko + str(poc)
+            pismenko = zoz[0]
+            poc = 0
+            taky_zoz = []
+        taky_zoz += [pismenko]
+        del zoz[0]
+    hotovecka += "\n"
+    kopia_fr.write(hotovecka)
 
 print("Pocet hier:", pocet_hier, "\nPocet krokov najdlhsej hry: ", max)
